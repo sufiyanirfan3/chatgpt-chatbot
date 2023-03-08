0506882cd9771c5a26f6a3bb25bb1a667fd65bc2
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2023-03-08 23:20:19 +0500

    prompts added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -66,8 +66,8 @@ function App() {
     <>
     <main class="main-container">
       <div class="container-fluid">
-        <div class="row"  style={{display:"flex",justifyContent:"flex-end"}}>
-          {/* <div class="col-12 col-md-5 col-lg-5">
+        <div class="row">
+          <div class="col-12 col-md-5 col-lg-5">
             <div class="accordion" id="accordionExample">
               <h3
                 style={{
@@ -88,7 +88,7 @@ function App() {
                     aria-expanded="false"
                     aria-controls="collapseOne"
                   >
-                    Audience Building Prompts
+                    Chocolate Chip Cookies
                   </button>
                 </h2>
                 <div
@@ -103,7 +103,7 @@ function App() {
                       write(0);
                     }}
                   >
-                    How do I make a viral tweet?
+                    What is the recipe for chocolate chip cookies?
                   </div>
                   <div
                     class="accordion-body question"
@@ -111,7 +111,7 @@ function App() {
                       write(1);
                     }}
                   >
-                    How do I increase my Twitter followers?
+                    How can I make my chocolate chip cookies chewy or crispy?
                   </div>
                   <div
                     class="accordion-body question"
@@ -119,24 +119,7 @@ function App() {
                       write(2);
                     }}
                   >
-                    What type of content should I post to grow my Instagram
-                    following?
-                  </div>
-                  <div
-                    class="accordion-body question"
-                    onClick={() => {
-                      write(3);
-                    }}
-                  >
-                    How do I edit my TikTok videos?
-                  </div>
-                  <div
-                    class="accordion-body question"
-                    onClick={() => {
-                      write(4);
-                    }}
-                  >
-                    How do I set up a YouTube account?
+                    What is the best type of chocolate to use in chocolate chip cookies?
                   </div>
                 </div>
               </div>
@@ -151,7 +134,7 @@ function App() {
                     aria-expanded="false"
                     aria-controls="collapseTwo"
                   >
-                    Copywriting Prompts
+                    Croissants
                   </button>
                 </h2>
                 <div
@@ -163,47 +146,26 @@ function App() {
                   <div
                     class="accordion-body question"
                     onClick={() => {
+                      write(3);
+                    }}
+                  >
+                    How do I make croissants from scratch?
+                  </div>
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(4);
+                    }}
+                  >
+                    What are some tips for making croissants flaky and buttery?
+                  </div>
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
                       write(5);
                     }}
                   >
-                    What is the AIDA model and how can it be used in
-                    copywriting?
-                  </div>
-                  <div
-                    class="accordion-body question"
-                    onClick={() => {
-                      write(6);
-                    }}
-                  >
-                    What is the difference between a feature and a benefit in
-                    copywriting?
-                  </div>
-                  <div
-                    class="accordion-body question"
-                    onClick={() => {
-                      write(7);
-                    }}
-                  >
-                    How can I use storytelling in my copywriting to connect with
-                    my audience?
-                  </div>
-                  <div
-                    class="accordion-body question"
-                    onClick={() => {
-                      write(8);
-                    }}
-                  >
-                    What are some best practices for writing headlines in
-                    copywriting?
-                  </div>
-                  <div
-                    class="accordion-body question"
-                    onClick={() => {
-                      write(9);
-                    }}
-                  >
-                    What is the difference between direct response copywriting
-                    and brand copywriting?
+                    Can I freeze croissants before baking them?
                   </div>
                 </div>
               </div>
@@ -218,7 +180,7 @@ function App() {
                     aria-expanded="false"
                     aria-controls="collapseThree"
                   >
-                    Email Marketing Prompts
+                    Bagels
                   </button>
                 </h2>
                 <div
@@ -230,11 +192,65 @@ function App() {
                   <div
                     class="accordion-body question"
                     onClick={() => {
+                      write(6);
+                    }}
+                  >
+                    How do I make bagels at home?
+                  </div>
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(7);
+                    }}
+                  >
+                    What are some toppings or flavors that go well with bagel?
+                  </div>
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(8);
+                    }}
+                  >
+                    Can I make bagels without a stand mixer?
+                  </div>
+
+                </div>
+              </div>
+
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingFour">
+                  <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseFour"
+                    aria-expanded="false"
+                    aria-controls="collapseFour"
+                  >
+                    Cupcakes
+                  </button>
+                </h2>
+                <div
+                  id="collapseFour"
+                  class="accordion-collapse collapse"
+                  aria-labelledby="headingFour"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(9);
+                    }}
+                  >
+                    What is the recipe for vanilla cupcakes?
+                  </div>
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
                       write(10);
                     }}
                   >
-                    What is email list building and why is it important for a
-                    business?
+                    How can I make my cupcakes moist and fluffy?
                   </div>
                   <div
                     class="accordion-body question"
@@ -242,15 +258,38 @@ function App() {
                       write(11);
                     }}
                   >
-                    How can I grow my email list quickly and effectively?
+                    What are some creative frosting ideas for cupcakes?
                   </div>
+
+                </div>
+              </div>
+
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingFive">
+                  <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseFive"
+                    aria-expanded="false"
+                    aria-controls="collapseFive"
+                  >
+                    Donuts
+                  </button>
+                </h2>
+                <div
+                  id="collapseFive"
+                  class="accordion-collapse collapse"
+                  aria-labelledby="headingFive"
+                  data-bs-parent="#accordionExample"
+                >
                   <div
                     class="accordion-body question"
                     onClick={() => {
                       write(12);
                     }}
                   >
-                    How can I get inspiration for email template designs?
+                    How do I make homemade donuts?
                   </div>
                   <div
                     class="accordion-body question"
@@ -258,7 +297,7 @@ function App() {
                       write(13);
                     }}
                   >
-                    What is a subject line in email marketing?
+                    What are some popular donut flavors and toppings?
                   </div>
                   <div
                     class="accordion-body question"
@@ -266,13 +305,248 @@ function App() {
                       write(14);
                     }}
                   >
-                    How can I use a mobile-responsive design for my email
-                    templates?
+                   Can I use a donut pan to make baked donuts?
                   </div>
+
+                </div>
+              </div>
+
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingSix">
+                  <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseSix"
+                    aria-expanded="false"
+                    aria-controls="collapseSix"
+                  >
+                    Cheesecake
+                  </button>
+                </h2>
+                <div
+                  id="collapseSix"
+                  class="accordion-collapse collapse"
+                  aria-labelledby="headingSix"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(15);
+                    }}
+                  >
+                    What is the recipe for classic cheesecake?
+                  </div>
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(16);
+                    }}
+                  >
+                    How can I make my cheesecake smooth and creamy?
+                  </div>
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(17);
+                    }}
+                  >
+                   What are some popular cheesecake flavors and toppings?
+                  </div>
+
+                </div>
+              </div>
+
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingSeven">
+                  <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseSeven"
+                    aria-expanded="false"
+                    aria-controls="collapseSeven"
+                  >
+                    Brownies
+                  </button>
+                </h2>
+                <div
+                  id="collapseSeven"
+                  class="accordion-collapse collapse"
+                  aria-labelledby="headingSeven"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(18);
+                    }}
+                  >
+                    What is the recipe for fudgy brownies?
+                  </div>
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(19);
+                    }}
+                  >
+                    How can I make my brownies more cakey or chewy?
+                  </div>
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(20);
+                    }}
+                  >
+                   What are some creative ways to customize brownies?
+                  </div>
+
+                </div>
+              </div>
+
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingEigth">
+                  <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseEigth"
+                    aria-expanded="false"
+                    aria-controls="collapseEigth"
+                  >
+                    Cinnamon Rolls
+                  </button>
+                </h2>
+                <div
+                  id="collapseEigth"
+                  class="accordion-collapse collapse"
+                  aria-labelledby="headingEigth"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(21);
+                    }}
+                  >
+                    How do I make cinnamon rolls from scratch?
+                  </div>
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(22);
+                    }}
+                  >
+                    What are some tips for making cinnamon rolls fluffy and gooey?
+                  </div>
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(23);
+                    }}
+                  >
+                   Can I make cinnamon rolls ahead of time?
+                  </div>
+
+                </div>
+              </div>
+
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingNinth">
+                  <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseNinth"
+                    aria-expanded="false"
+                    aria-controls="collapseNinth"
+                  >
+                    Apple Pie
+                  </button>
+                </h2>
+                <div
+                  id="collapseNinth"
+                  class="accordion-collapse collapse"
+                  aria-labelledby="headingNinth"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(24);
+                    }}
+                  >
+                    What is the recipe for apple pie?
+                  </div>
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(25);
+                    }}
+                  >
+                    How can I make my apple pie filling not too runny?
+                  </div>
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(26);
+                    }}
+                  >
+                   What are some ways to decorate the crust of an apple pie?
+                  </div>
+
+                </div>
+              </div>
+
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTenth">
+                  <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseTenth"
+                    aria-expanded="false"
+                    aria-controls="collapseTenth"
+                  >
+                   Banana Bread
+                  </button>
+                </h2>
+                <div
+                  id="collapseTenth"
+                  class="accordion-collapse collapse"
+                  aria-labelledby="headingTenth"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(27);
+                    }}
+                  >
+                    What is the recipe for banana bread?
+                  </div>
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(28);
+                    }}
+                  >
+                    How can I make my banana bread moist and flavorful?
+                  </div>
+                  <div
+                    class="accordion-body question"
+                    onClick={() => {
+                      write(29);
+                    }}
+                  >
+                   Can I add other ingredients to my banana bread, like nuts or chocolate chips?
+                  </div>
+
                 </div>
               </div>
             </div>
-          </div> */}
+          </div>
 
           <div class="col-12 col-md-7 col-lg-7">
             <div class="container">
