3fbe546d21b3600bc44c2c2d6f7d657a00885352
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2023-03-08 20:34:07 +0500

    .

--- server/server.js
+++ server/server.js
@@ -25,26 +25,19 @@ app.post('/chat', async (req, res) => {
   try {
     const question = req.body.question;
 
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt:`Respond to questions in a friendly chat style.The question is: ${question}. Remove un-necessary question marks.`,
-      temperature: 0.7,
-      max_tokens: 2048,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-      stop: [" AI:"," Human:"],
-      // model: "text-davinci-003",
-      // prompt: `${question}`,
-      // temperature: 0, // Higher values means the model will take more risks.
-      // max_tokens: 2048, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      // top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      // frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      // presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        
+        {role: "user", content:question}
+      ],
     });
 
+    var output=response.data.choices[0].message.content
+    output=output.trim()
+
     res.status(200).send({
-      answer: response.data.choices[0].text,
+      answer: output,
       question:question
     });
 
